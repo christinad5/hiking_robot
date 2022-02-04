3e6a456967ecb0d44a099fc64ef5b90c6b258733
Christina Dong <cdab2018@mymail.pomona.edu>
2022-02-04 12:32:47 -0800

    basic_humanoid_sim.py loads humanoid and plane urdf and writes quaternion orientation to csv file

--- basic_humanoid_sim.py
+++ basic_humanoid_sim.py
@@ -1,20 +1,24 @@
 import pybullet as p
 import time
 import pybullet_data
+import csv
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-10)
+p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("/data/plane.urdf")
 startPos = [0,0,1]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("/data/humanoid.urdf", startPos, startOrientation)
 
 p.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-for i in range(10000):
+output_file = open('orientation_data.csv', 'w')
+datawriter = csv.writer(output_file)
+for i in range(100):
+    cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+    datawriter.writerows([cubeOrn])
     p.stepSimulation()
     time.sleep(1./240.)
 
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos, cubeOrn)
+output_file.close()
 p.disconnect()
